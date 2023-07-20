717c2f74b644a545b868b8763932d2239c2e6078
jiaezzang <visveryver2@gmail.com>
2023-07-20 14:19:40 +0900

    activeElements atom관리

--- src/atoms/atoms.ts
+++ src/atoms/atoms.ts
@@ -1,6 +1,10 @@
 import { atom } from 'jotai';
 import Editor from '../editor/core';
+import { GroupElement, WrapElement } from '../editor/elements';
+type TDMElements = WrapElement | GroupElement;
 
 export const editorAtom = atom<Editor | null>(null);
 
 export const testEditorAtom = atom<Editor | null>(null);
+
+export const ActiveElementsAtom = atom<TDMElements[]>([])
