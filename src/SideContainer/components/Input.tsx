8e69b9c8c35e719389a0689f9704c183896422c2
jiaezzang <visveryver2@gmail.com>
2023-07-20 14:19:10 +0900

    format input 컴포넌트

---
+++ src/SideContainer/components/Input.tsx
@@ -0,0 +1,25 @@
+import { ChangeEventHandler } from 'react';
+
+type input = {
+    title: string;
+    name?: string;
+    value: number | undefined;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+    readOnly?: boolean;
+};
+export default function Input({ title, name, value, onChange, readOnly }: input) {
+    return (
+        <div className="flex flex-col">
+            <label className="text-[0.75rem] text-gray-500">{title}</label>
+            <input
+                type="number"
+                className="w-16 text-right px-1"
+                step="any"
+                name={name}
+                value={value ? Math.round(value * 100) / 100 : 0}
+                onChange={onChange}
+                readOnly={readOnly}
+            ></input>
+        </div>
+    );
+}
